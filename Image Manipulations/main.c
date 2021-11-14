9ed74932231cd609128a2701a3700f8559a02eb2
OrkunAvci <orkunavci28@gmail.com>
2021-11-14 20:41:14 +0300

    Update main.c

--- Image Manipulations/main.c
+++ Image Manipulations/main.c
@@ -9,11 +9,13 @@ int main()
 {
 	PGM* pgm = read_pgm("D:\\GitHub\\Image Processing\\Read PGM\\LanaDelRey.pgm");
 
-	spatial_lowpass(pgm);
+	PGM* output;
+	mirror_y(pgm);
 
 	write_pgm(pgm);
 
 	clean_up_pgm(pgm);
+	//clean_up_pgm(output);
 
 	system("pause");
 	return 0;
